29c874bcc32e1d4133ed1e5f166bcd699cbe7e7b
倪泽恒 <644508159@qq.com>
2020-04-07 16:30:29 +0800

    优化登录日志。

--- Nzh.Knight.IService/Permissions/IUserService.cs
+++ Nzh.Knight.IService/Permissions/IUserService.cs
@@ -10,18 +10,11 @@ namespace Nzh.Knight.IService
     public interface IUserService : IBaseService<UserModel>
     {
         UserModel GetDetail(int Id);
+
         UserModel CheckLogin(string username, string password);
-        /// <summary>
-        /// 修改密码
-        /// </summary>
-        /// <param name="model">密码实体</param>
-        /// <returns></returns>
+
         bool ModifyPwd(PassWordModel model);
-        /// <summary>
-        /// 重置密码
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+
         bool InitPwd(UserModel model);
     }
 }
